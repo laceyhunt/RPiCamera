728678f368be33dd4d431be22da31a12d11c5739
lacey <hunt3247@vandals.uidaho.edu>
2023-08-10 16:32:21 -0700

    capture testing

--- imageWithClick.py
+++ imageWithClick.py
@@ -1,16 +1,21 @@
+# captures video from webcam, continuously displays it,
+#   and saves a snapshot as an image when 'q' is pressed
 import cv2
 
+cap = cv2.VideoCapture(0)       # create video capture using default camera (USB camera)
 
-cap = cv2.VideoCapture(0)
+while(True):                    # start live frame loop
+    ret, frame = cap.read()     # frame = current video feed frame
+                                #   ret is bool value to determine success
+    # rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA) # converts BGR to BGRA
 
-while(True):
-    ret, frame = cap.read()
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    cv2.imshow('frame')         # display current frame
+                                #   also pass 'rgb' if conversion occurs ^
+    if cv2.waitKey(1) & 0xFF == ord('q'):   # wait for key press every 1ms
+                                            #   bitmask for returned ASCII value of pressed key
+                                            #   and verify if 'q' was pressed
+        out = cv2.imwrite('capture.jpg', frame) # saves frame as image
+        break                   # exit loop and end
 
-    cv2.imshow('frame', rgb)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        out = cv2.imwrite('capture.jpg', frame)
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+cap.release()                   # get rid of object to free up camera for other apps
+cv2.destroyAllWindows()         # close OpenCV windows
