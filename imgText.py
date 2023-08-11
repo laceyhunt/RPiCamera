523600be73ef126182a1d977cfebba6b491239ff
lacey <hunt3247@vandals.uidaho.edu>
2023-08-11 14:57:50 -0700

    date, file name, and directory change made

--- imgText.py
+++ imgText.py
@@ -4,15 +4,15 @@ import os
 #path = r'/home/pi/RPiCamera/capture.jpg'
 image = cv2.imread("capture.jpg")
 window_name = 'Image'
-text = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
+text = datetime.datetime.now().strftime("%d-%b-%Y %H:%M:%S")
 print(text)
 font = cv2.FONT_HERSHEY_COMPLEX
-org=(00,185)
+org=(00,1000)
 fontScale=1
-color = (255,0,0)     # function to decide between black and white?
-thickness=2
+color = (0,0,0)     # function to decide between black and white?
+thickness=1
 directory = "/home/pi/RPiCamera/images"
-image = cv2.putText(image,text,org,font,fontScale,color,thickness,cv2.LINE_AA,True)
+image = cv2.putText(image,text,org,font,fontScale,color,thickness,cv2.LINE_AA,False)
 #image = cv2.putText(image, text, org, font, fontScale,color, thickness, cv2.LINE_AA, True) 
 cv2.imshow(window_name,image)
 os.chdir(directory)
