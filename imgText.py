d3387dc76c3866d6134cf73d00bfe014eb1de77d
lacey <hunt3247@vandals.uidaho.edu>
2023-08-11 14:38:01 -0700

    save images to /images with date time text

--- imgText.py
+++ imgText.py
@@ -1,5 +1,6 @@
 import cv2
 import datetime
+import os
 #path = r'/home/pi/RPiCamera/capture.jpg'
 image = cv2.imread("capture.jpg")
 window_name = 'Image'
@@ -10,10 +11,16 @@ org=(00,185)
 fontScale=1
 color = (255,0,0)     # function to decide between black and white?
 thickness=2
-image = cv2.putText(image,text,org,font,fontScale,color,thickness,cv2.LINE_AA,False)
+directory = "/images"
+image = cv2.putText(image,text,org,font,fontScale,color,thickness,cv2.LINE_AA,True)
 #image = cv2.putText(image, text, org, font, fontScale,color, thickness, cv2.LINE_AA, True) 
 cv2.imshow(window_name,image)
+os.chdir(directory)
+print("Before saving image:")  
+print(os.listdir(directory)) 
 cv2.imwrite(text+".jpg",image)
+print("After saving image:")  
+print(os.listdir(directory)) 
 # Wait for the user to press a key
 cv2.waitKey(0)
  
