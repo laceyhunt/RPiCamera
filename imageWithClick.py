67532e0bd9243a47a11ec028bd33621cd12cfdb5
lacey <hunt3247@vandals.uidaho.edu>
2023-08-11 15:23:34 -0700

    more fixes

--- imageWithClick.py
+++ imageWithClick.py
@@ -5,19 +5,19 @@ import datetime
 import os
 
 cap = cv2.VideoCapture(0)       # create video capture using default camera (USB camera)
-window_name = 'Image'
+# window_name = 'Image'
 
 while(True):                    # start live frame loop
-    ret, window_name = cap.read()     # frame = current video feed frame
+    ret, frame = cap.read()     # frame = current video feed frame
                                 #   ret is bool value to determine success
     # rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA) # converts BGR to BGRA
 
-    cv2.imshow('frame',window_name)         # display current frame
+    cv2.imshow('frame',frame)         # display current frame
                                 #   also pass 'rgb' if conversion occurs ^
     if cv2.waitKey(1) & 0xFF == ord('q'):   # wait for key press every 1ms
                                             #   bitmask for returned ASCII value of pressed key
                                             #   and verify if 'q' was pressed
-        out = cv2.imwrite('capture.jpg', window_name) # saves frame as image
+        out = cv2.imwrite('capture.jpg', frame) # saves frame as image
         break                   # exit loop and end
 
 cap.release()                   # get rid of object to free up camera for other apps
@@ -29,10 +29,10 @@ font = cv2.FONT_HERSHEY_COMPLEX
 org=(00,1000)
 fontScale=1
 color = (0,0,0)     # function to decide between black and white?
-thickness=1.5
+thickness=1
 directory = "/home/pi/RPiCamera/images"
 image = cv2.putText(image,text,org,font,fontScale,color,thickness,cv2.LINE_AA,False)
-cv2.imshow(window_name,image)
+# cv2.imshow(window_name,image)
 os.chdir(directory)
 print("Before saving image:")  
 print(os.listdir(directory)) 
