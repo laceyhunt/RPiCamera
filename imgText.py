ec833d0f6201250e114f306136fccaee79d75607
lacey <hunt3247@vandals.uidaho.edu>
2023-08-11 13:24:15 -0700

    adding text test

---
+++ imgText.py
@@ -0,0 +1,14 @@
+import cv2
+import datetime
+path = r'capture.jpg'
+image = cv2.imread(path)
+window_name = 'Image'
+text = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
+font = cv2.FONT_HERSHEY_COMPLEX
+org=(00,185)
+fontScale=1
+color = (0,0,0)     # function to decide between black and white?
+thickness=2
+image = cv2.putText(image,text,org,font,fontScale,color,thickness,cv2.LINE_AA,False)
+image = cv2.putText(image, text, org, font, fontScale,color, thickness, cv2.LINE_AA, True) 
+cv2.imshow(window_name,image)
